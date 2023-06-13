cc5727e1ba87c630e53430336ab6ea2d8a7891ca
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-06-13 16:48:37 +0700

    update

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:salesappnew/screens/home/widgets/menu_list.dart';
 
 // ignore: must_be_immutable
 class HomeScreen extends StatelessWidget {
@@ -8,32 +9,113 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: const Icon(Icons.apps_outlined),
         backgroundColor: Colors.red,
-        title: const Row(
+        title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            Expanded(
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Opacity(
+                    opacity: 0.5,
+                    child: Text(
+                      "Semangat Pagi",
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Text(
+                    "Ryan Hadi Dermawan",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ],
+              ),
+            ),
+            Row(
               children: [
-                Opacity(
-                  opacity: 0.5,
-                  child: Text(
-                    "Semangat Pagi",
-                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                CircleAvatar(
+                  backgroundColor: Colors.red[300],
+                  radius: 20,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.add_alert_outlined,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-                Text(
-                  "Ryan Hadi Dermawan",
-                  style: TextStyle(fontSize: 16),
+                SizedBox(
+                  width: 10,
+                ),
+                CircleAvatar(
+                  radius: 20,
+                  backgroundImage: NetworkImage(
+                      'https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg'),
                 ),
               ],
-            ),
-            Text("foto"),
+            )
           ],
         ),
         centerTitle: true,
       ),
-      body: Text("Home"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 18,
+          vertical: 10,
+        ),
+        child: ListView(
+          children: [
+            const Text(
+              "Menu",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 17,
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  HomeMenuList(title: "visit", primary: true),
+                  HomeMenuList(title: "Callsheet"),
+                  HomeMenuList(title: "Invoice"),
+                  HomeMenuList(title: "Order"),
+                  HomeMenuList(title: "Item"),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  "Info Promo",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                  ),
+                ),
+                const Text(
+                  "See More",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 13,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/screens/home/widgets/menu_list.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomeMenuList extends StatelessWidget {
+  final String title;
+  bool primary = false;
+
+  HomeMenuList({required this.title, bool? primary}) : super() {
+    if (primary != null) {
+      this.primary = primary;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        print("ke menu");
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(
+          bottom: 5,
+          top: 5,
+          right: 15,
+          left: 5,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                color: primary ? Colors.red[400] : Colors.grey[300],
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              title,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[600],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
