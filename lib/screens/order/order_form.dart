7fd1f79583856764e9c3a1a4d92b9d9dd6d03ad1
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-07-07 16:29:10 +0700

    update

--- lib/screens/order/order_form.dart
+++ lib/screens/order/order_form.dart
@@ -43,7 +43,7 @@ class OrderFormScreen extends StatelessWidget {
                     BackButtonCustom(),
                     const Row(
                       children: [
-                        Icon(Icons.currency_exchange, size: 17),
+                        Icon(Icons.assignment, size: 17),
                         Padding(
                           padding: EdgeInsets.symmetric(horizontal: 3),
                           child: Text(
