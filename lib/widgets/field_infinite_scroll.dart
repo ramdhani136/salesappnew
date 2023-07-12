977512ff844fc0da1a7dffb1104ed7260fd00e40
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-07-12 12:31:20 +0700

    update

--- lib/widgets/field_infinite_scroll.dart
+++ lib/widgets/field_infinite_scroll.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
+import 'package:salesappnew/bloc/contact/contact_bloc.dart';
+import 'package:salesappnew/screens/contact/contact_form.dart';
 
 class FieldInfiniteScroll extends StatelessWidget {
   String? placeholder;
@@ -88,47 +91,279 @@ class FieldInfiniteScroll extends StatelessWidget {
             ],
           ),
         ),
-        Container(
-            width: Get.width * 0.95,
-            height: 46,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(4),
-              border: Border.all(
-                color: valid
-                    ? const Color.fromARGB(255, 182, 182, 182)
-                    : Colors.red,
-                width: 1.0,
+        InkWell(
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) => CustomerList(),
+            );
+          },
+          child: Container(
+              width: Get.width * 0.95,
+              height: 46,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(4),
+                border: Border.all(
+                  color: valid
+                      ? const Color.fromARGB(255, 182, 182, 182)
+                      : Colors.red,
+                  width: 1.0,
+                ),
               ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        "Administrator",
+                        style: TextStyle(
+                          color: disabled ? Colors.grey[800] : Colors.grey[900],
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.close,
+                      color: Colors.grey,
+                    ),
+                    iconSize: 20,
+                  ),
+                ],
+              )),
+        )
+      ],
+    );
+  }
+}
+
+class CustomerList extends StatefulWidget {
+  const CustomerList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CustomerList> createState() => _CustomerFormState();
+}
+
+class _CustomerFormState extends State<CustomerList> {
+  TextEditingController customerC = TextEditingController();
+  TextEditingController picC = TextEditingController();
+  TextEditingController phonC = TextEditingController();
+  // bool isPicEmpty = true;
+  // bool isPoneEmpty = true;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   customerC =
+  //       TextEditingController(text: "${widget.visitState.data.customer?.name}");
+  //   picC = TextEditingController();
+  //   phonC = TextEditingController();
+
+  //   picC.addListener(() {
+  //     setState(() {
+  //       isPicEmpty = picC.text.isEmpty;
+  //     });
+  //   });
+
+  //   phonC.addListener(() {
+  //     setState(() {
+  //       isPoneEmpty = phonC.text.isEmpty;
+  //     });
+  //   });
+  // }
+
+  @override
+  void dispose() {
+    // customerC.dispose();
+    // picC.dispose();
+    // phonC.dispose();
+    super.dispose();
+  }
+
+  ContactBloc bloc = ContactBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: FractionallySizedBox(
+        widthFactor: 1.15,
+        child: Container(
+          width: Get.width * 0.95,
+          height: Get.height,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      "Administrator",
+                    const Text(
+                      "Customer List",
                       style: TextStyle(
-                        color: disabled ? Colors.grey[800] : Colors.grey[900],
+                        color: Color.fromARGB(255, 66, 66, 66),
+                        fontWeight: FontWeight.bold,
                         fontSize: 16,
                       ),
                     ),
+                    IconButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      icon: const Icon(Icons.close),
+                    ),
                   ],
                 ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.close,
-                    color: Colors.grey,
+                const SizedBox(height: 20),
+                BlocBuilder<ContactBloc, ContactState>(
+                  bloc: bloc,
+                  builder: (context, state) {
+                    picC.text = bloc.pic;
+                    phonC.text = bloc.phone;
+
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Customer :",
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: customerC,
+                          enabled: false,
+                          autocorrect: false,
+                          enableSuggestions: false,
+                          decoration: InputDecoration(
+                            hintStyle: TextStyle(color: Colors.grey[300]),
+                            hintText: "Cth : CV Jaya Abadi",
+                            contentPadding:
+                                const EdgeInsets.symmetric(horizontal: 10),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          "PIC :",
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: picC,
+                          // enabled: (VisitC.status.value == "0" ||
+                          //     VisitC.status.value == "1"),
+                          autocorrect: false,
+                          enableSuggestions: false,
+                          decoration: InputDecoration(
+                            hintStyle: TextStyle(color: Colors.grey[300]),
+                            hintText: "Cth : Ilham",
+                            contentPadding:
+                                const EdgeInsets.symmetric(horizontal: 10),
+                            // enabledBorder: picC.text.isEmpty
+                            //     ? const OutlineInputBorder(
+                            //         borderSide: BorderSide(
+                            //           color: Colors.red,
+                            //           width: 1,
+                            //         ),
+                            //       )
+                            //     : null,
+                            border: const OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.blue,
+                                width: 1.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          "Phone :",
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: phonC,
+                          keyboardType: TextInputType.number,
+                          autocorrect: false,
+                          enableSuggestions: false,
+                          decoration: InputDecoration(
+                            hintStyle: TextStyle(color: Colors.grey[300]),
+                            hintText: "Contoh: 089637428874",
+                            contentPadding:
+                                const EdgeInsets.symmetric(horizontal: 10),
+                            // enabledBorder: phonC.text.isEmpty
+                            //     ? const OutlineInputBorder(
+                            //         borderSide: BorderSide(
+                            //           color: Colors.red,
+                            //           width: 1,
+                            //         ),
+                            //       )
+                            //     : null,
+                            border: const OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.blue,
+                                width: 1.0,
+                              ),
+                            ),
+                            suffixIcon: IconButton(
+                              onPressed: () async {
+                                bloc.add(ContactGetPhone());
+                              },
+                              icon: const Icon(
+                                Icons.contact_phone,
+                                color: Colors.grey,
+                                size: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                const SizedBox(height: 30),
+                SizedBox(
+                  width: Get.width,
+                  height: 46,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 65, 170, 69),
+                    ),
+                    onPressed: () async {
+                      // widget.contactBloc.add(
+                      //   ContactInsertData(
+                      //     data: {
+                      //       "name": picC.text,
+                      //       "phone": phonC.text,
+                      //       "customer": widget.visitState.data.customer!.id
+                      //     },
+                      //   ),
+                      // );
+                      // widget.contactBloc.add(
+                      //   GetListInput(
+                      //       customerId: widget.visitState.data.customer!.id),
+                      // );
+                    },
+                    child: const Text("Save"),
                   ),
-                  iconSize: 20,
                 ),
               ],
-            ))
-      ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
